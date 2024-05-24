e0c286d9333b93b38071a2d555494f60db419c4d
Michal Puchy <puchy.michal@gmail.com>
2024-05-24 16:13:19 +0200

    5.7 Blog List Frontend, step 7; blog details view or not

--- src/App.jsx
+++ src/App.jsx
@@ -91,9 +91,7 @@ const App = () => {
         </div>
       }
       <h1>blogs</h1>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      { blogs.map(blog => <Blog key={ blog.id } blog={ blog } />) }
     </div>
   )
 }
--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,39 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [view, setView] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const handleView = (event) => {
+    setView(!view)
+  }
+
+  return (
+    <div style={ blogStyle } >
+      <div>
+        { blog.title } { blog.author } 
+        <button onClick={ handleView }>
+          { view ? 'hide' : 'view' }
+        </button>
+      </div>
+      { view && (
+        <>
+          <div>{ blog.url }</div>
+          <div>
+            { blog.likes } <button>likes</button>
+          </div>
+          <div>{ blog.author }</div>
+        </>
+      ) }
+    </div>  
+  )
+}
 
 export default Blog
